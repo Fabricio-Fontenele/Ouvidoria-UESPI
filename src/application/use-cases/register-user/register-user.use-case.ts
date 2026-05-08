249401824dde3177fd81a00bb85c159167b31484
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-05-08 00:32:24 -0300

    feat: add user registration domain and use case

---
+++ src/application/use-cases/register-user/register-user.use-case.ts
@@ -0,0 +1,65 @@
+import { User, UserRole } from '#src/domain/entities/user.js'
+import { Email } from '#src/domain/value-objects/email.js'
+import { Name } from '#src/domain/value-objects/name.js'
+import { PlainPassword } from '#src/domain/value-objects/password.js'
+
+import { UserAlreadyExistsError } from './errors/user-already-exists-error.js'
+import type { PasswordHasher } from '../../cryptography/password-hasher.js'
+import type { UserRepository } from '../../repositories/users-repository.js'
+import type { UseCase } from '../use-case.js'
+
+interface RegisterUserInput {
+  name: string
+  email: string
+  password: string
+}
+
+interface RegisterUserOutput {
+  user: {
+    id: string
+    name: string
+    email: string
+    role: string
+    createdAt: string
+  }
+}
+
+export class RegisterUserUseCase implements UseCase<RegisterUserInput, RegisterUserOutput> {
+  constructor(
+    private readonly usersRepository: UserRepository,
+    private readonly passwordHasher: PasswordHasher,
+  ) {}
+
+  async execute({ name, email, password }: RegisterUserInput): Promise<RegisterUserOutput> {
+    const normalizedName = Name.create(name)
+    const normalizedEmail = Email.create(email)
+    const plainPassword = PlainPassword.create(password)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(normalizedEmail.getValue())
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    const hashedPassword = await this.passwordHasher.hash(plainPassword.getValue())
+
+    const user = User.create({
+      name: normalizedName,
+      email: normalizedEmail,
+      passwordHash: hashedPassword,
+      role: UserRole.PROTESTER,
+    })
+
+    await this.usersRepository.save(user)
+
+    return {
+      user: {
+        id: user.id.toString(),
+        name: user.name.getValue(),
+        email: user.email.getValue(),
+        role: user.role.toString(),
+        createdAt: user.createdAt.toString(),
+      },
+    }
+  }
+}
