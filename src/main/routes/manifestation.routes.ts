0a6bbfe4facdae915140bf4f2b46b733deaec434
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-05-16 12:42:50 -0300

    feat: wire controller factories, routes and fastify bootstrap

---
+++ src/main/routes/manifestation.routes.ts
@@ -0,0 +1,53 @@
+import type { FastifyInstance } from 'fastify'
+
+import { UserRole } from '#src/domain/entities/user.js'
+import { adaptRoute } from '#src/infra/http/fastify/fastify-route-adapter.js'
+import {
+  ensureAuthenticated,
+  optionalAuthenticate,
+  requireRoles,
+} from '#src/infra/http/fastify/middlewares/auth-middleware.js'
+
+import {
+  makeAddManifestationMessageController,
+  makeFinalizeManifestationController,
+  makeGetManifestationDetailsController,
+  makeListUserManifestationsController,
+  makeRegisterManifestationController,
+  makeTrackManifestationByProtocolController,
+} from '../factories/controllers/manifestation.js'
+
+export async function registerManifestationRoutes(app: FastifyInstance): Promise<void> {
+  app.post('/manifestations/track', adaptRoute(makeTrackManifestationByProtocolController()))
+
+  app.post('/manifestations', { preHandler: optionalAuthenticate }, adaptRoute(makeRegisterManifestationController()))
+
+  const authenticatedManifestant: ReadonlyArray<typeof ensureAuthenticated> = [
+    ensureAuthenticated,
+    requireRoles(UserRole.MANIFESTANT),
+  ]
+
+  app.get(
+    '/manifestations',
+    { preHandler: [...authenticatedManifestant] },
+    adaptRoute(makeListUserManifestationsController()),
+  )
+
+  app.get(
+    '/manifestations/:manifestationId',
+    { preHandler: [...authenticatedManifestant] },
+    adaptRoute(makeGetManifestationDetailsController()),
+  )
+
+  app.post(
+    '/manifestations/:manifestationId/messages',
+    { preHandler: [...authenticatedManifestant] },
+    adaptRoute(makeAddManifestationMessageController()),
+  )
+
+  app.post(
+    '/manifestations/:manifestationId/finalize',
+    { preHandler: [...authenticatedManifestant] },
+    adaptRoute(makeFinalizeManifestationController()),
+  )
+}
