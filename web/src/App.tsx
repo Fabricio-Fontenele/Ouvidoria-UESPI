4d2c7147d59f007d6f42e339fbe9ebde0972ba3f
Kaua-cel <kauaneres.dev@gmail.com>
2026-05-15 22:49:04 -0300

    doc: add react framework in project

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Testando...</h1>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
