e0d4d6f9e341453055339ec3cc360aa50d7dd3ea
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-05-16 13:00:39 -0300

    fix: switch prisma client to driver adapter for runtime db url

--- src/infra/database/prisma/client.ts
+++ src/infra/database/prisma/client.ts
@@ -1,5 +1,14 @@
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+const connectionString = process.env['DATABASE_URL']
+
+if (connectionString === undefined || connectionString === '') {
+  throw new Error('DATABASE_URL must be set before the Prisma client is created')
+}
+
+export const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString }),
+})
 
 export type PrismaTransactionClient = Parameters<Parameters<typeof prisma.$transaction>[0]>[0]
